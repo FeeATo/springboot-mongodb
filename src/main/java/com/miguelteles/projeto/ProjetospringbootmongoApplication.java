90fbe41c7e745ca9f808cf4a0e7b4fd4105dacc9
Miguel Teles <miguelteles2002@hotmail.com>
2022-09-19 20:06:48 -0300

    Primeiro commit, projeto criado

---
+++ src/main/java/com/miguelteles/projeto/ProjetospringbootmongoApplication.java
@@ -0,0 +1,13 @@
+package com.miguelteles.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetospringbootmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetospringbootmongoApplication.class, args);
+	}
+
+}
