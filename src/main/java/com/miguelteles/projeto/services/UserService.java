c411c54c516074c2e54df6e2625fb928df9b7fdf
Miguel Teles <miguelteles2002@hotmail.com>
2022-09-22 22:25:28 -0300

    Obtendo um usuário por ID

--- src/main/java/com/miguelteles/projeto/services/UserService.java
+++ src/main/java/com/miguelteles/projeto/services/UserService.java
@@ -1,12 +1,14 @@
 package com.miguelteles.projeto.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.miguelteles.projeto.domain.User;
 import com.miguelteles.projeto.repository.UserRepository;
+import com.miguelteles.projeto.services.exception.ObjectNotFoundException;
 
 @Service //fala pro spring que isso é um Service
 public class UserService {
@@ -17,6 +19,12 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll(); //esta função vem do repositório do spring data, eu não criei
+	}
+	
+	public User findById(String id) {
+		//Optional is a variable that may or may not have a non-null value
+		Optional<User> user = repo.findById(id); //esta função vem do spring
+		return user.orElseThrow(()->new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
 }
