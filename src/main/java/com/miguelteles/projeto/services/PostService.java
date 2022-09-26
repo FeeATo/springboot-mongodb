6cabe9819b85d798fb1751c62ac858f980fcbe2d
Miguel Teles <miguelteles2002@hotmail.com>
2022-09-25 22:10:28 -0300

    Obtendo post por ID

---
+++ src/main/java/com/miguelteles/projeto/services/PostService.java
@@ -0,0 +1,22 @@
+package com.miguelteles.projeto.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.miguelteles.projeto.domain.Post;
+import com.miguelteles.projeto.repository.PostRepository;
+import com.miguelteles.projeto.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> post = repo.findById(id);
+		return post.orElseThrow(()->new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
