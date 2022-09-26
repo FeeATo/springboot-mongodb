c0cbbc888c0d87602b829c51fcfc49f09600820d
Miguel Teles <miguelteles2002@hotmail.com>
2022-09-25 21:46:46 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/miguelteles/projeto/config/Instantiation.java
+++ src/main/java/com/miguelteles/projeto/config/Instantiation.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.miguelteles.projeto.DTO.AuthorDTO;
 import com.miguelteles.projeto.domain.Post;
 import com.miguelteles.projeto.domain.User;
 import com.miguelteles.projeto.repository.PostRepository;
@@ -34,10 +35,12 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria); //salva no banco duas cópias diferentes de 'maria', não é uma referência
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
 		
-		userRepository.saveAll(Arrays.asList(maria,alex,bob)); //salva no banco
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria)); //salva no banco duas cópias diferentes de 'maria', não é uma referência
+		
+		
 		postRepository.saveAll(Arrays.asList(post1,post2)); //salva no banco
 	}
 
